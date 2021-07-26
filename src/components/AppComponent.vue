1416baa646ebfafa8a98a184c259c76b24236ff3
= <bio_ahmet@mail.ru>
2021-07-26 15:37:03 +0600

    random words from json file

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -3,11 +3,14 @@
     <h1 class="display-4">Speed Writing Competition</h1>
     <p class="lead">Improve your Typing Speed</p>
     <hr class="my-4" />
-
+    <div v-if="isFinish" class="alert alert-primary">
+      <h1>Game Over</h1>
+    </div>
+    <div v-else>
     <div class="card">
       <div class="card-body">
         <span
-          v-for="(word, key) in words"
+          v-for="(word, key) in words.filter((data, index) => index < 20)"
           :key="key"
           :class="key != 0 || wordControl"
           class="ms-2 fs-3"
@@ -20,48 +23,78 @@
         <div class="input-group input-group-lg">
           <input type="text" class="form-control" v-model="writingWord" />
           <button class="btn btn-light" disabled type="button">
-            1:00
+            {{timer}} sec
           </button>
-          <button class="btn btn-light" type="button">
+          <button :disabled='isRunning' class="btn btn-light" type="button" @click="getWords">
             Refresh
           </button>
         </div>
       </div>
     </div>
+    </div>
   </div>
 </template>
 
 <script>
+import wordList from '@/assets/wordlist.json'
 export default {
   data () {
     return {
-      words: [
-        'ucak',
-        'lale',
-        'bilgisayar',
-        'kalem',
-        'araba',
-        'okumak',
-        'telefon',
-        'almanya'
-      ],
+      words: [],
       writingWord: null,
-      isTrue: true
+      isTrue: true,
+      trueCount: 0,
+      falseCount: 0,
+      timer: 60,
+      interval: false,
+      isRunning: false,
+      isFinish: false,
+      wordList: wordList
     }
   },
   watch: {
     writingWord (val) {
+      if (!val || val === ' ') {
+        this.writingWord = ''
+        return
+      }
+      if (!this.isRunning) this.toggleTimer()
       const word = this.words[0].slice(0, val.length)
-      if (word === val) {
-        console.log('dogru')
-      } else {
-        this.isTrue = false
+      const usersWord = val.trim()
+      this.isTrue = word === usersWord
+      if (val.indexOf(' ') !== -1) {
+        this.isTrue ? this.trueCount++ : this.falseCount++
+        this.words.splice(0, 1)
+        this.writingWord = ''
+        this.isTrue = true
       }
     }
   },
   computed: {
     wordControl () {
       return this.isTrue ? 'focused-word' : 'focused-word bg-danger'
+    }
+  },
+
+  mounted () {
+    this.getWords()
+  },
+
+  methods: {
+    getWords () {
+      this.words = this.wordList.sort(() => Math.random() - 0.5).splice(0, 300)
+    },
+    toggleTimer () {
+      this.isRunning = true
+      this.interval = setInterval(this.timeProcess, 1000)
+    },
+    timeProcess () {
+      if (this.timer === 0) {
+        clearInterval(this.interval)
+        this.isFinish = true
+        return
+      }
+      this.timer--
     }
   }
 }
