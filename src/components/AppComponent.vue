76c6d9f761ab820f5790c5ecaaee0feba0f16c42
= <bio_ahmet@mail.ru>
2021-07-26 17:41:49 +0600

    app is ready

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -4,33 +4,47 @@
     <p class="lead">Improve your Typing Speed</p>
     <hr class="my-4" />
     <div v-if="isFinish" class="alert alert-primary">
-      <h1>Game Over</h1>
+      <h3>Time Is Over</h3>
+      <p class="display-4">
+        {{ wpm }} WPM <small class="fs-5">(words per minute)</small>
+      </p>
+      <span>Accuracy : {{ accuracy }} % </span> <br />
+      <span>Correct words : {{ trueCount }} </span> <br />
+      <span>Wrong words : {{ falseCount }} </span> <br />
+      <button @click="newGame" class="btn btn-success btn-lg mt-5">
+        New Game
+      </button>
     </div>
     <div v-else>
-    <div class="card">
-      <div class="card-body">
-        <span
-          v-for="(word, key) in words.filter((data, index) => index < 20)"
-          :key="key"
-          :class="key != 0 || wordControl"
-          class="ms-2 fs-3"
-          >{{ word }}</span
-        >
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-body bg-secondary">
-        <div class="input-group input-group-lg">
-          <input type="text" class="form-control" v-model="writingWord" />
-          <button class="btn btn-light" disabled type="button">
-            {{timer}} sec
-          </button>
-          <button :disabled='isRunning' class="btn btn-light" type="button" @click="getWords">
-            Refresh
-          </button>
+      <div class="card">
+        <div class="card-body">
+          <span
+            v-for="(word, key) in words.filter((data, index) => index < 20)"
+            :key="key"
+            :class="key != 0 || wordControl"
+            class="ms-2 fs-3"
+            >{{ word }}</span
+          >
         </div>
       </div>
-    </div>
+      <div class="card">
+        <div class="card-body bg-secondary">
+          <div class="input-group input-group-lg">
+            <input type="text" class="form-control" v-model="writingWord" />
+            <button class="btn btn-light" disabled type="button">
+              {{ timer }} sec
+            </button>
+            <button
+              :disabled="isRunning"
+              class="btn btn-light"
+              type="button"
+              @click="getWords"
+            >
+              Refresh
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -73,6 +87,14 @@ export default {
   computed: {
     wordControl () {
       return this.isTrue ? 'focused-word' : 'focused-word bg-danger'
+    },
+    wpm () {
+      return 300 - this.words.length
+    },
+    accuracy () {
+      const percent = 100 / this.wpm
+      const val = percent * this.trueCount
+      return isNaN(val) ? 0 : val.toFixed(2)
     }
   },
 
@@ -81,6 +103,16 @@ export default {
   },
 
   methods: {
+    newGame () {
+      this.getWords()
+      this.isFinish = false
+      this.timer = 60
+      this.isTrue = true
+      this.isRunning = false
+      this.writingWord = ''
+      this.trueCount = 0
+      this.falseCount = 0
+    },
     getWords () {
       this.words = this.wordList.sort(() => Math.random() - 0.5).splice(0, 300)
     },
